0c228fa47b4b84f8754fdd26c07ef1a63ef82426
amric-amz <amric@amzonestep.com>
2022-03-08 21:42:23 +0500

    mubashir first commit forget passsword and reset password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,18 +34,18 @@
                 <x-jet-input id="password" class="block mt-1 w-full form-control" type="password" name="password" required autocomplete="current-password" />
             </div>
 
-{{--            <div class="block mt-4">--}}
-{{--                <label for="remember_me" class="flex items-center">--}}
-{{--                    <x-jet-checkbox id="remember_me" name="remember" />--}}
-{{--                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>--}}
-{{--                </label>--}}
-{{--            </div>--}}
+           <div class="block mt-4">
+               <label for="remember_me" class="flex items-center">
+                   <x-jet-checkbox id="remember_me" name="remember" />
+                   <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+               </label>
+            </div>
 
-{{--            @if (Route::has('password.request'))--}}
-{{--                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">--}}
-{{--                    {{ __('Forgot your password?') }}--}}
-{{--                </a>--}}
-{{--            @endif--}}
+            @if (Route::has('password.request'))
+              <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                  {{ __('Forgot your password?') }}
+              </a>
+           @endif
 
             <div class="flex items-center justify-end mt-4">
                 <x-jet-button class="btn-form-submit my-4 text-center">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -771,6 +771,10 @@
 {{--                                    <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>--}}
 {{--                                    <span class="ml-2">Logout </span>--}}
                                     <!-- Authentication -->
+                                    <a href="{{ route('profile.show') }}">
+                                        <i class="fa fa-user"></i>
+                                        <span class="nav-text">Profile</span>
+                                    </a>
                                     <form method="POST" action="{{ route('logout') }}">
                                         @csrf
 
@@ -783,6 +787,12 @@
 {{--                                </a>--}}
                             </div>
                         </li>
+                        {{-- <li>
+                            <a href="{{ route('profile.show') }}">
+                                <i class="fa fa-user"></i>
+                                <span class="nav-text">Profile</span>
+                            </a>
+                        </li> --}}
                     </ul>
                 </div>
             </nav>
@@ -838,12 +848,12 @@
                         <span class="nav-text">Walmart</span>
                     </a>
                 </li>
-                <li>
+                {{-- <li>
                     <a href="{{ route('profile.show') }}">
                         <i class="fa fa-user"></i>
                         <span class="nav-text">Profile</span>
                     </a>
-                </li>
+                </li> --}}
 {{--                <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">--}}
 {{--                        <i class="flaticon-381-internet"></i>--}}
 {{--                        <span class="nav-text">Bootstrap</span>--}}
